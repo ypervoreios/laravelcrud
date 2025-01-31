5176d0a3ae600012760cf86185e5769fd09e0adf
ypervoreios <c.chorinos@gmail.com>
2025-01-31 17:43:37 +0200

    v.0.1

---
+++ resources/views/users/index.blade.php
@@ -0,0 +1,15 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Users</title>
+</head>
+<body>
+    <h2>User Page</h2>
+    <ul>
+        <li>
+            Users here
+        </li>
+    </ul>
+</body>
+</html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,12 +2,12 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>My first laravel project</title>
+    <title>Laravel project</title>
 </head>
 <body>
-    <h1>Welcome to my first laravel project</h1>
-    <p>This is the first laravel project I have created.</p>
-    <p>Click the button bellow to see the list of users:</p>
-    <a href="" class="btn">Find users</a>
+    <h1>Welcome to my first Laravel project</h1>
+    <p>This is the first Laravel project I have created.</p>
+    <p>Click the button below to see the list of users:</p>
+    <a href="/users" class="btn">Find users</a>
 </body>
 </html>
